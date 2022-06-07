601b8944f820347fa9328887fb3d4588a6bb3e07
kdoni <chrisvdoni@gmail.com>
2022-06-07 10:15:38 +0300

    Playing with toggle buttons.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const board = document.querySelector(".board");
-for (let i=0;i<4096;i++){
+for (let i=1;i<4096;i++){
     var pixel = document.createElement("div");
     pixel.classList.add("pixel");
     board.appendChild(pixel);
-    console.log(board)
 }
