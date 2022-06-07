0b24d987a2070e53da72bba567bb42ce3528c885
kdoni <chrisvdoni@gmail.com>
2022-06-07 11:16:43 +0300

    making progress

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const toggle = document.querySelector(".toggleBtn input");
+toggle.addEventListener("click", () =>{
+    const onOff = toggle.parentNode.querySelector(".onOff");
+    onOff.textContent = toggle.checked ? "Drawing: ON" : " Drawing: OFF";
+})
+
 const board = document.querySelector(".board");
-for (let i=1;i<4096;i++){
+for (let i=0;i<4096;i++){
     var pixel = document.createElement("div");
     pixel.classList.add("pixel");
     board.appendChild(pixel);
