236902a67f1089ee48f086b575db3b5ee4d16139
kdoni <chrisvdoni@gmail.com>
2022-06-07 22:14:28 +0300

    stagnating....

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ toggle.addEventListener("click", () =>{
 
 const board = document.querySelector(".board");
 for (let i=0;i<4096;i++){
-    var pixel = document.createElement("div");
+    const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     board.appendChild(pixel);
 }
