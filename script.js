cc73d06d628557387405bc06c71c03a18ce69181
kdoni <chrisvdoni@gmail.com>
2022-06-08 21:59:19 +0300

    Building on Javascript Knowledge: Pausing project.

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ toggle.addEventListener("click", () =>{
     const onOff = toggle.parentNode.querySelector(".onOff");
     onOff.textContent = toggle.checked ? "Drawing: ON" : " Drawing: OFF";
 })
-
+const pixels = [];
 const board = document.querySelector(".board");
 for (let i=0;i<4096;i++){
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     board.appendChild(pixel);
+    pixels.push(pixel);
 }
+
