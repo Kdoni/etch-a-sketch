4b55e0be9e7b05f0185589a46c641cc144520356
kdoni <chrisvdoni@gmail.com>
2022-08-17 14:21:20 +0300

    Finally managed the rainbow color thingy...

--- myscript.js
+++ myscript.js
@@ -3,17 +3,22 @@
 const grid = document.getElementsByClassName("item");
 for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener("mouseover", (e) => {
-        let value = document.getElementById("favcolor");
-        e.target.style.backgroundColor = value.value;
+        if (pride.classList.contains("clicked")) {
+            for (let x = 0; x < rainbow.length; x++) {
+                grid[i].style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+            }
+        } else {
+            let value = document.getElementById("favcolor");
+            e.target.style.backgroundColor = value.value;
+        }
     });
 }
 
 //Applying the rainbow color.
 
 let rainbow = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"];
-let pride = document.querySelector(".pride");
-// pride.addEventListener("click", (e) => {
-//     for (let i = 0; i < rainbow.length; i++) {
-//         e.target.style.backgroundColor = rainbow[i];
-//     }
-// })
+var pride = document.querySelector(".prido");
+pride.addEventListener("click", (e) => {
+    pride.classList.toggle("clicked");
+
+});
