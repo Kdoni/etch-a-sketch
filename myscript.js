8f7ae25151f71d6e382e72ca85b7c8d9e51e0709
kdoni <chrisvdoni@gmail.com>
2022-08-17 15:48:35 +0300

    Making progress.

--- myscript.js
+++ myscript.js
@@ -7,6 +7,8 @@ for (let i = 0; i < grid.length; i++) {
             for (let x = 0; x < rainbow.length; x++) {
                 grid[i].style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
             }
+        } else if (eraser.classList.contains("toggled")) {
+            grid[i].style.backgroundColor = "white";
         } else {
             let value = document.getElementById("favcolor");
             e.target.style.backgroundColor = value.value;
@@ -19,6 +21,37 @@ for (let i = 0; i < grid.length; i++) {
 let rainbow = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"];
 var pride = document.querySelector(".prido");
 pride.addEventListener("click", (e) => {
+    eraser.classList.remove("toggled");
     pride.classList.toggle("clicked");
 
+});
+
+//Resetting the color;
+
+var reset = document.querySelector(".reseto");
+reset.addEventListener("click", (e) => {
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].style.backgroundColor = "white";
+    }
+})
+
+//Eraser settings.
+
+var eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", (e) => {
+    pride.classList.remove("clicked");
+    eraser.classList.toggle("toggled");
+    // if (eraser.classList.contains("toggled")) {
+    //     grid.addEventListener("mouseover", (e) => {
+    //         e.style.backgroundColor = "white";
+    //     })
+    // }
+})
+
+/*Just to remove classes from other elements*/
+
+var palette = document.querySelector(".palette");
+palette.addEventListener("click", (e) => {
+    pride.classList.remove("clicked");
+    eraser.classList.remove("toggled");
 });
