f9111d541a2004b1ca47f7e15647a0a2c6c0d37c
kdoni <chrisvdoni@gmail.com>
2022-08-18 00:21:59 +0300

    Mostly done.

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
-//Simple brushing through the grid container
+//Defining global variables.
 
 const picasso = document.querySelector(".drawing");
+var dimensions = document.querySelector(".dimensions");
+var numerons = document.querySelector("input[type=range]");
+
+//Dynamically setting up the rows and columns of the grid-container.
+
+
+
+//Simple brushing through the grid container
+
 const grid = document.getElementsByClassName("item");
 for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener("mouseover", (e) => {
@@ -57,13 +66,58 @@ palette.addEventListener("click", (e) => {
     eraser.classList.remove("toggled");
 });
 
-//Dividing the drawing board into multiple divs//
-var dimensions = document.querySelector(".dimensions");
-var numerons = document.querySelector("input[type=range]");
+//Displays the text range of the slider.
+
 dimensions.innerHTML = `${numerons.value} X ${numerons.value}`;
 
-numerons.addEventListener("input", (e) => {
+
+
+//Dividing the drawing board into multiple divs
+
+function populateGrid() {
     dimensions.innerHTML = `${numerons.value} X ${numerons.value}`;
-});
+    picasso.style.gridTemplateColumns = `repeat(${numerons.value},1fr)`;
+    picasso.style.gridTemplateRows = `repeat(${numerons.value},1fr)`;
+    while (picasso.firstChild) {
+        picasso.removeChild(picasso.firstChild)
+    }
+    for (let i = 0; i < numerons.value ** 2; i++) {
+        const item = document.createElement("div")
+        item.setAttribute("class", "item");
+        picasso.appendChild(item);
+        item.addEventListener("mouseover", (e) => {
+            if (pride.classList.contains("clicked")) {
+                for (let x = 0; x < rainbow.length; x++) {
+                    item.style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+                }
+            } else if (eraser.classList.contains("toggled")) {
+                item.style.backgroundColor = "white";
+            } else {
+                let value = document.getElementById("favcolor");
+                e.target.style.backgroundColor = value.value;
+            }
+        });
+    }
+}
+populateGrid();
 
+numerons.addEventListener("change", (e) => {
+    populateGrid();
+})
 
+//When clicking on a grid-item it should progressively darken
+
+// function LightenDarkenColor(col, amt) {
+//     var num = parseInt(col, 16);
+//     var r = (num >> 16) + amt;
+//     var b = ((num >> 8) & 0x00FF) + amt;
+//     var g = (num & 0x0000FF) + amt;
+//     var newColor = g | (b << 8) | (r << 16);
+//     return newColor.toString(16);
+// }
+
+// var darken = document.querySelector(".item");
+// darken.addEventListener("click", (e) => {
+//     let value = document.getElementById("favcolor");
+//     LightenDarkenColor(value.value, -40);
+// })
